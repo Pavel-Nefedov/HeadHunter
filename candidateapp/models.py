20a5538965ac0fac58d3145f2a466d345ee64fc9
pashka <pavel.nefyodov.74@mail.ru>
2022-12-15 12:48:35 +0500

    Добавил приложение личного кабинета кандидата candidateapp и базовый HTML для проверки его работоспособности

---
+++ candidateapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Candidate(models.Model):
+    """
+    search_area - район/область поиска работы
+    """
+    # user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    # profile_pic = models.ImageField(null=True, blank=True, upload_to="images/profile/")
+    email = models.EmailField(max_length=128)
+    phone_number = models.CharField(max_length=12)
+    search_area = models.CharField(verbose_name='адрес', max_length=256)
