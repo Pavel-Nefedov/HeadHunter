7cf711e669b6e1325fc67f543779cb7d3bd433b5
79268 <jhgjvhg>
2022-12-21 23:12:17 +0300

    Vacancy model added

--- companyapp/models.py
+++ companyapp/models.py
@@ -13,9 +13,11 @@ class Company(models.Model):
     phone_number = models.CharField(max_length=12)
 
 
+
 class CompanyProfile(models.Model):
     company = models.OneToOneField(Company, unique=True, null=False, db_index=True, on_delete=models.CASCADE)
     about = models.CharField(blank=True, max_length=512, verbose_name='обо мне')
+
 
     @receiver(post_save, sender=Company)
     def create_user_profile(sender, instance, created, **kwargs):
@@ -25,3 +27,32 @@ class CompanyProfile(models.Model):
     @receiver(post_save, sender=Company)
     def save_user_profile(sender, instance, **kwargs):
         instance.shopuserprofile.save()
+
+
+class Vacancy(models.Model):
+    about_company = models.CharField(blank=True, max_length=512, verbose_name='Обязанности')
+    duties_description = models.CharField(blank=True, max_length=512, verbose_name='Обязанности')
+    requirements_description = models.CharField(blank=True, max_length=512, verbose_name='Требования')
+    work_conditions = models.CharField(blank=True, max_length=512, verbose_name='Условия труда')
+    created = models.DateTimeField(auto_now_add=True)
+    salary_min = models.CharField(blank=True, max_length=512, verbose_name='Минимальная заралата')
+    salary_max = models.CharField(blank=True, max_length=512, verbose_name='Максимальная заралата')
+
+    is_for_disabled = models.BooleanField(
+        default=False,
+        verbose_name='Вакансия для людей с инвалидностью',
+        help_text=('Отметьте, вакансия подходит для людей с ограниченными физическими возможностями')
+    )
+
+    is_full_day = models.BooleanField(
+        default=False,
+        verbose_name='Полный рабочий день',
+        help_text=('Отметьте, если вакансия полного рабочего дня')
+    )
+    is_intern = models.BooleanField(
+        default=False,
+        verbose_name='Cтажировка',
+        help_text=('Отметьте, если вакансия является стажировкой')
+    )
+
+
