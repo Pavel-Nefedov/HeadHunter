a0d6ea011c7affddae71868fe80e5bcd9166638d
Roman <slyusarrv@yandex.ru>
2022-12-22 13:46:09 +0500

    add only python_container

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ "$DB_TYPE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $DB_HOST $DB_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+make flush
+make makemigrations
+make migrate
+make createsuperuser
+
+exec "$@"
