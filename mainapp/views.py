9f35ab825645407e9e02d3215972cb719af76f58
NIck <nezgovorov.nikita@gmail.com>
2023-01-06 16:35:10 +0300

    -add frontend for vacancy

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from companyapp.models import Vacancy
 
 from mainapp.models import News
 
 
 class Index(TemplateView):
     template_name = 'mainapp/index.html'
+    extra_context = {'vacancies': Vacancy.objects.all().order_by('-created')[:5]}
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
