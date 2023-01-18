90dba74b7fc89ec3a98d0b5ff44df99e794f8862
79268 <jhgjvhg>
2023-01-18 23:10:35 +0300

    #HH-dev company profile views added

--- companyapp/urls.py
+++ companyapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-
 from companyapp.views import CompanyK, CompanyLK, VacancySearch
+
+
+
 
 app_name = 'companyapp'
 
 urlpatterns = [
-    path('company_profile/<int:pk>/', CompanyLK.as_view(), name='company_profile'),
+    path('company_profile/<int:pk>/',  CompanyLK.as_view(), name='company_profile'),
     path('company_lk/', CompanyK.as_view(), name='company_lk'),
-    path('vacancy_search/', VacancySearch.as_view(), name='vacancy_search'),
+    path('vacancy_search/',  VacancySearch.as_view(), name='vacancy_search'),
 ]
--- companyapp/views.py
+++ companyapp/views.py
@@ -1,13 +1,54 @@
-from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.views import View
+from django.views.generic import TemplateView, CreateView,View
+from companyapp.forms import CompanyProfileForm
+from companyapp.models import CompanyProfile
 
 
 class CompanyLK(TemplateView):
     template_name = 'companyapp/company.html'
 
 
-class CompanyK(TemplateView):
-    template_name = 'companyapp/company_lk.html'
+# class CompanyK(TemplateView):
+#     template_name = 'companyapp/company_lk.html'
 
 
 class VacancySearch(TemplateView):
     template_name = 'companyapp/vacancy_search.html'
+
+
+class CompanyK(View):
+
+    def get(self, request):
+        if request.method == 'POST':
+            form = CompanyProfileForm(data=request.POST, files=request.FILES, instance=request.user)
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect(reverse('company_lk'))
+        else:
+            form = CompanyProfileForm(instance=request.user)
+
+        context = {
+            'title': 'Профиль',
+            'form': form,
+        }
+        return render(request, 'companyapp/company_lk.html', context)
+
+
+
+# def CompanyK(request):
+#     if request.method == 'POST':
+#         form = CompanyProfileForm(data=request.POST, files=request.FILES, instance=request.user)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('company_lk'))
+#     else:
+#         form = CompanyProfileForm(instance=request.user)
+#
+#     context = {
+#         'title': 'Профиль',
+#         'form': form,
+#     }
+#     return render(request, 'companyapp/company_lk.html', context)
