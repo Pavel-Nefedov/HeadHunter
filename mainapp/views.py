36f1ba58673933af1537d95ddf6fba5fe2fb757d
NIck <nezgovorov.nikita@gmail.com>
2023-01-19 18:49:17 +0300

    -add vacancy cards on main page

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,14 +1,17 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 from companyapp.models import Vacancy
 from mainapp.models import News
 
 
 class Index(TemplateView):
+    paginate_by = 5
     template_name = 'mainapp/index.html'
-    extra_context = {'vacancies': Vacancy.objects.all().order_by('-created')[:5]}
+    # extra_context = {'vacancies': Vacancy.objects.all().order_by('-created')[:5]}
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
         data['news'] = News.objects.all().order_by('-pk')[:3]
+        data['vacancies'] = Vacancy.objects.filter(is_active=True).order_by('-created')[:5]
         return data
+
