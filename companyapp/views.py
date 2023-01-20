e4029b6c43868d6fba8b6fb2f37f1ef57e66b177
NIck <nezgovorov.nikita@gmail.com>
2023-01-20 19:23:47 +0300

    -add vacancy create page -add vacancy update page

--- companyapp/views.py
+++ companyapp/views.py
@@ -1,11 +1,12 @@
-from django.views.generic import TemplateView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, DetailView, UpdateView
 
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views import View
 from django.views.generic import TemplateView, CreateView, View
-from companyapp.forms import CompanyProfileForm
+from companyapp.forms import CompanyProfileForm, VacancyForm
 from companyapp.models import CompanyProfile, Vacancy
 
 
@@ -35,7 +36,44 @@ class VacancyView(DetailView):
     def get_context_data(self, pk=None, **kwargs):
         context = super(DetailView, self).get_context_data(**kwargs)
         context['vacancies'] = Vacancy.objects.filter(pk=pk)
+        context['user'] = self.request.user
+        context['company'] = self.object.company
         return context
+
+
+class VacancyCreate(LoginRequiredMixin, CreateView):
+    template_name = 'companyapp/vacancy_create.html'
+    model = Vacancy
+    fields = ['vacancy_name', 'city', 'duties_description', 'requirements_description', 'work_conditions', 'salary_min',
+              'salary_max', 'currency', 'is_for_disabled', 'is_full_day', 'is_intern']
+    form_model = VacancyForm
+
+    def get_success_url(self):
+        return reverse('companyapp:vacancy', kwargs={'pk': self.object.id})
+
+    def form_valid(self, form):
+        form.instance.company = CompanyProfile.objects.get(user=self.request.user)
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['company'] = CompanyProfile.objects.get(user=self.request.user)
+        return data
+
+
+class VacancyUpdate(LoginRequiredMixin, UpdateView):
+    template_name = 'companyapp/vacancy_edit.html'
+    model = Vacancy
+    fields = ['vacancy_name', 'city', 'duties_description', 'requirements_description', 'work_conditions', 'salary_min',
+              'salary_max', 'currency', 'is_for_disabled', 'is_full_day', 'is_intern']
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['company'] = CompanyProfile.objects.get(user=self.request.user)
+        return data
+
+    def get_success_url(self):
+        return reverse('companyapp:vacancy', kwargs={'pk': self.object.id})
 
 
 class CompanyK(View):
@@ -69,3 +107,4 @@ class CompanyK(View):
 #         'form': form,
 #     }
 #     return render(request, 'companyapp/company_lk.html', context)
+
