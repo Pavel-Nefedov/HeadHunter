10ba8d08be77ce4c727f81e2be19f84d4ee8e69f
Roman <slyusarrv@yandex.ru>
2023-01-31 12:18:59 +0500

    added canvas for lk moderator

--- moderatorapp/views.py
+++ moderatorapp/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 
+from mainapp.mixins import IsModeratorCheckMixin
 
-class ModeratorLkIndex(TemplateView):
+
+@method_decorator(login_required, name='dispatch')
+class ModeratorLkIndex(IsModeratorCheckMixin, TemplateView):
     template_name = 'moderatorapp/index.html'
+
+
