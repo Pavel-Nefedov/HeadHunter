11daa9c1a3cadbe4086dfee5971ea66fb02fa204
@maks-r <maks.rus.in@mail.ru>
2023-02-06 22:35:08 +0300

    update

--- companyapp/urls.py
+++ companyapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from companyapp.views import (CompanyProfileCreateView,
-                              CompanyProfileUpdateView, CompProfile,
-                              PartnerCompanyView, ResumeSearch, VacancyCreate,
-                              VacancyUpdate, VacancyView)
+                              CompanyProfileUpdateView,
+                              PartnerCompanyView,
+                              ResumeSearch, VacancyCreate, VacancyUpdate,
+                              VacancyView,CompProfile, VacanciesList)
 
 app_name = 'companyapp'
 
@@ -13,9 +14,11 @@ urlpatterns = [
     path('company_profile/compprofile/', CompProfile.as_view(), name='company_profile'),
     path('company_profile/create/', CompanyProfileCreateView.as_view(), name='company_create'),
     path('company_profile/update/<int:pk>/', CompanyProfileUpdateView.as_view(), name='company_update'),
+    path('comp_vacancies/', VacanciesList.as_view(), name='vac_list'),
     path('vacancy/create/', VacancyCreate.as_view(), name='vacancy_create'),
     path('vacancy/edit/<int:pk>/', VacancyUpdate.as_view(), name='vacancy_edit'),
     path('vacancy/<int:pk>/', VacancyView.as_view(), name='vacancy'),
     path('resume_search/',  ResumeSearch.as_view(), name='resume_search'),
+    path('favorites/',  VacancyView.as_view(), name='favorites'),
     path('partner/<int:pk>/', PartnerCompanyView.as_view(), name='partner'),
 ]
