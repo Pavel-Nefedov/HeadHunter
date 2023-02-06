97758197cd6e63c37f2042c7b64562dff1ee2dee
@maks-r <maks.rus.in@mail.ru>
2023-02-06 19:08:30 +0300

    fix create resume

--- candidateapp/forms.py
+++ candidateapp/forms.py
@@ -7,7 +7,11 @@ class ResumeForm(forms.ModelForm):
 
     class Meta:
         model = Resume
-        fields = ('candidate', 'contact_info', 'position_and_salary',
-                  'work_experience', 'education', 'advanced_training')
-
+        fields = ['candidate', 'name', 'surname', 'patronymic',
+                  'birthday', 'city', 'gender', 'moving',
+                  'business_trips', 'desired_position', 'salary', 'busyness',
+                  'work_schedule', 'getting_started', 'end_work', 'working',
+                  'organization', 'post', 'responsibilities', 'skills',
+                  'about_me', 'level', 'educational_institution', 'faculty', 'specialization', 'year_graduation',
+                  'course_name', 'organization_conducted', 'specialization_course', 'year_graduation_course']
 
