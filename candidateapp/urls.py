7c4e7d52d99295d7e318f6eebdc4aaa91c911136
@maks-r <maks.rus.in@mail.ru>
2023-02-06 14:58:33 +0300

    update create resume

--- candidateapp/urls.py
+++ candidateapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 
 import candidateapp.views as candidateapp
 
-from .views import (CandidateMain, FormVacancySearch, ResumeCreateView,
-                    ResumeDeleteView, ShowProfileUpdateView,
+from .views import (CandidateMain, FormVacancySearch,
+                    ShowProfileUpdateView,
                     ShowResumeDetailView, VacancySearch)
 
 app_name = 'candidateapp'
@@ -18,9 +18,10 @@ urlpatterns = [
 
     # path('resume/<int:pk>/', VacancyView.as_view(), name='resume'),
     path('resume/', candidateapp.resume, name='resume'),
+    # path('resume_create/', ResumeCreateView.as_view(), name='resume_create'),
     path('resume_update/<int:pk>/', candidateapp.resume_update, name='resume_update'),
-    path('resume_create/', ResumeCreateView.as_view(), name='resume_create'),
-    path('resume_delete/<int:pk>/', ResumeDeleteView.as_view(), name='resume_delete'),
+    path('resume_create/', candidateapp.resume_create, name='resume_create'),
+    path('resume_delete/<int:pk>/', candidateapp.resume_delete, name='resume_delete'),
     # path('resume_detail/', candidateapp.resume_detail, name='resume_detail'),
     # path('resume/<int:pk>/', ShowResumePageView.as_view(), name='resume'),
     path('resume_detail/<int:pk>/', ShowResumeDetailView.as_view(), name='resume_detail'),
@@ -28,5 +29,4 @@ urlpatterns = [
     path('update/<int:pk>/', ShowProfileUpdateView.as_view(), name='update_profile'),
     path('vacancy_search/',  VacancySearch.as_view(), name='vacancy_search'),
     path('search/',  FormVacancySearch.as_view(), name='form_vacancy_search'),
-
 ]
